bd624a60124e15a26df9c29a5b92a426fcc246f5
codingisverycool <116462701+codingisverycool@users.noreply.github.com>
2025-07-30 20:16:35 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,6 +5,3 @@ app = Flask(__name__)
 @app.route('/api/hello')
 def hello():
     return jsonify({"message": "Hello from Flask backend!"})
-    
-def handler(environ, start_response):
-    return app(environ, start_response)
